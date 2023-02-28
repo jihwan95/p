8f1e47c3f8aee652987842db912f22c3161676d2
jihwan95 <pwjerry2002@gmail.com>
2023-02-28 17:37:15 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const person = {
+    name:{
+        firstname:"Jihwan",
+        lastname:"Oh"
+    },
+    likes:["apple", "samsung"],
+    printhello:function(){
+        return "hello";
+    }
+}
+
+console.log(person["printhello"]());
